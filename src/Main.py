0ab34e93038488fe3f5edc6bc485d7308afb21ba
SeaBass <sthiem@email.arizona.edu>
2019-06-28 15:20:41 -0700

    fixed stability plots

--- src/Main.py
+++ src/Main.py
@@ -14,7 +14,7 @@ import numpy as np
 
 # quantitatively analyze how much the models predictions are changing 
 # as more data is used to label the samples
-def measure_stability(reupsample=False, online_weight_adjust=True):
+def measure_stability(reupsample=False, online_weight_adjust=True, online_confidence_constant=0.25):
 
     # init weighted dict
     w = WD()
@@ -26,11 +26,11 @@ def measure_stability(reupsample=False, online_weight_adjust=True):
 
     # keep track of current and previous dataframes
     data_ss_labeled_prev = None
-    data_ss_labeled_curr = None 
+    data_ss_labeled_curr = pd.DataFrame() 
 
     # every round add a hundred samples, but the first few should be smaller incremenets
     # this array represents that
-    sample_counts = np.concatenate([[1, 5, 15, 25, 50, 75], np.arange(100, data_labeled.shape[0], 50)])
+    sample_counts = np.arange(100, data_labeled.shape[0]+1, 100)
 
     # initialize frame to store stability info
     stability = pd.DataFrame(index=['SC', 'HW', 'SW'], columns=sample_counts)
@@ -40,26 +40,27 @@ def measure_stability(reupsample=False, online_weight_adjust=True):
         print(sample_count, "...")
 
         if(online_weight_adjust):
-            addr_data = '../data/data-ss-labeled_'+str(sample_count)+'(ON).csv'
+            addr_data = '../data/data-ss-labeled_'+str(sample_count)+'(ON-'+str(online_confidence_constant)+').csv'
         else:
             addr_data = '../data/data-ss-labeled_'+str(sample_count)+'(OFF).csv'
         
         # set the current as the previous
-        data_ss_labeled_prev = data_ss_labeled_curr
+        data_ss_labeled_prev = data_ss_labeled_curr.copy()
 
         # get the semi-supervised labeled data
         if(reupsample):
-            data_ss_labeled_curr = w.upsample(data_labeled, data_unlabeled, sample_count, addr_newlabels=addr_data, online_weight_adjust=online_weight_adjust)
+            data_ss_labeled_curr = w.upsample(data_labeled, data_unlabeled, sample_count, addr_newlabels=addr_data, online_weight_adjust=online_weight_adjust, online_confidence_constant=online_confidence_constant)
         else:
             # try to read if the data is there, if its not regenerate it
             try:
                 data_ss_labeled_curr = pd.read_csv(addr_data)
             except:
-                data_ss_labeled_curr = w.upsample(data_labeled, data_unlabeled, sample_count, addr_newlabels=addr_data, online_weight_adjust=online_weight_adjust)
+                data_ss_labeled_curr = w.upsample(data_labeled, data_unlabeled, sample_count, addr_newlabels=addr_data, online_weight_adjust=online_weight_adjust, online_confidence_constant=online_confidence_constant)
             
         total_same_SC = 0
         total_same_HW = 0
         total_same_SW = 0
+        total_comp_samples = 0
         
         # Check that both previous and current dataframes are valid
         # they wont be on the first round so just keep 0's in total same
@@ -68,21 +69,36 @@ def measure_stability(reupsample=False, online_weight_adjust=True):
             # loop through each sample index and compare the two
             for i in range(data_ss_labeled_curr.shape[0]):
 
-                # The prediction is the same as last time then increment the respective counter
-                if(data_ss_labeled_curr.loc[i, 'SC'] == data_ss_labeled_prev.loc[i, 'SC']):
-                    total_same_SC += 1
-                if(data_ss_labeled_curr.loc[i, 'HW'] == data_ss_labeled_prev.loc[i, 'HW']):
-                    total_same_HW += 1
-                if(data_ss_labeled_curr.loc[i, 'SW'] == data_ss_labeled_prev.loc[i, 'SW']):
-                    total_same_SW += 1
+                # don't bother on samples that are not computer at all
+                if(data_ss_labeled_curr.loc[i, 'SC'] != 0 or data_ss_labeled_curr.loc[i, 'HW'] != 0 or data_ss_labeled_curr.loc[i, 'SW'] != 0):
+                    
+                    # The prediction is the same as last time then increment the respective counter
+                    if(data_ss_labeled_curr.loc[i, 'SC'] == data_ss_labeled_prev.loc[i, 'SC']):
+                        total_same_SC += 1
+                    if(data_ss_labeled_curr.loc[i, 'HW'] == data_ss_labeled_prev.loc[i, 'HW']):
+                        total_same_HW += 1
+                    if(data_ss_labeled_curr.loc[i, 'SW'] == data_ss_labeled_prev.loc[i, 'SW']):
+                        total_same_SW += 1
+
+                    total_comp_samples += 1
 
         # store the stability information gathered
-        stability.loc['SC', sample_count] = float(total_same_SC) / data_ss_labeled_curr.shape[0]
-        stability.loc['HW', sample_count] = float(total_same_HW) / data_ss_labeled_curr.shape[0]
-        stability.loc['SW', sample_count] = float(total_same_SW) / data_ss_labeled_curr.shape[0]
+        if(total_comp_samples > 0):
+            stability.loc['SC', sample_count] = float(total_same_SC) / total_comp_samples
+            stability.loc['HW', sample_count] = float(total_same_HW) / total_comp_samples
+            stability.loc['SW', sample_count] = float(total_same_SW) / total_comp_samples
+        else:
+            stability.loc['SC', sample_count] = -3.1415926
+            stability.loc['HW', sample_count] = -3.1415926
+            stability.loc['SW', sample_count] = -3.1415926
 
     # store the resultant info locally
-    stability.to_csv('../data/stability.csv')
+    if online_weight_adjust:
+        fileNameOut = '../data/stability(ON-'+str(online_confidence_constant)+').csv'
+    else:
+        fileNameOut = '../data/stability(OFF).csv'
+    
+    stability.to_csv(fileNameOut)
 
     # return the data frame
     return stability
@@ -171,17 +187,28 @@ def test_performance(alpha=0, fold_count=8, addr_out='../data/test_out.csv'):
 
 def sweep_alphas(alpha_min, alpha_max, alpha_delta, fold_count=8):
 
+    print('Sweep sigmoid alphas in classification.')
+
     for alpha in range(alpha_min, alpha_max, alpha_delta):
 
         print('\t --- Alpha:', alpha)
         test_performance(alpha=alpha, addr_out='../data/'+str(fold_count)+'-fold_X-val_alpha-'+str(alpha)+'.csv')
 
+def sweep_confidence_online_weight_adjust(alpha_min, alpha_max, alpha_delta):
+
+    print('Sweeping online confidence constants...')
+    alpha = alpha_min
+    while(alpha <= alpha_max):
+        print('\t --- Alpha:', alpha)
+        measure_stability(reupsample=True, online_weight_adjust=True, online_confidence_constant=alpha)
+        alpha += alpha_delta
+
 def main():
 
-    # TODO: FIX STABILITY PLOT
-
     #measure_stability(reupsample=True, online_weight_adjust=True)
-    #measure_stability(reupsample=True, online_weight_adjust=False)
+    measure_stability(reupsample=True, online_weight_adjust=False)
+
+    sweep_confidence_online_weight_adjust(0.0, 0.15, 0.05)
 
     #show_top_keywords()
 
@@ -190,7 +217,7 @@ def main():
     #sweep_alphas(5, 40, 5)
 
     
-    Labeler().run()
+    #Labeler().run()
 
     #data_labeled = pd.read_csv('../data/recall_labeled.csv')
     #data_ss_labeled = pd.read_csv('../data/data-ss-labeled_800.csv')
